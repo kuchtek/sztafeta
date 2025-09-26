e3f0f3d47befaa1de526044f0d6f81de69ab4397
Marcin Kuchczynski <kum@revolgy.com>
2025-09-26 21:55:59 +0200

    feat: change line ending

--- app.py
+++ app.py
@@ -353,7 +353,7 @@ def process_activities():
     app.logger.debug("Total distance: " + str(total_distance))
     # czas na pobranie notatek, obrazków
     notes = request.form.get("notes")
-    str_builder += f"\n {notes} \n Wpis dodany za pomocą https://hejto.sztafetastat.eu \n {tag_community}"
+    str_builder += f"</br> {notes} </br> Wpis dodany za pomocą https://hejto.sztafetastat.eu </br> {tag_community}"
     if(community in ['ksiezycowy-spacer', 'sport', 'rozwoj']):
         str_builder = str_builder.replace('+','-')
     files = request.files.getlist('files')
